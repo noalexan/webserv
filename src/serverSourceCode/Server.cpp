987ea37a1acd794e2314b9b7282620fcc364bd49
marwan0620@gmail.com <marwan0620@gmail.com>
2023-07-09 15:23:32 +0200

    all setup of server : first try of making responses, and parsing headers for texts and img (icon is failed) (server in testing)

---
+++ src/serverSourceCode/Server.cpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Server.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mayoub <mayoub@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/24 13:37:59 by sihemayoub        #+#    #+#             */
+/*   Updated: 2023/07/03 13:34:08 by mayoub           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Server.hpp"
+
+struct Server	ServerConstruct( int domain, int service, int protocole, u_long interface, int port, int backlog, void (*launch)(struct Server *server) ) {
+
+	struct Server	server;
+
+	server.domain = domain;
+	server.service = service;
+	server.protocole = protocole;
+	server.interface = interface;
+	server.port = port;
+	server.backlog = backlog;
+
+	server.address.sin_family = domain;
+	server.address.sin_port = htons(port);
+	server.address.sin_addr.s_addr = htonl(interface);
+
+	server.sockfd = socket(domain, service, protocole);
+	if (server.sockfd <= 0) {
+		std::cerr << "Socket error" << std::endl;
+		exit(ERROR_EXIT);
+	}
+
+	int on = 1;
+	if (setsockopt(server.sockfd, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(on)) == -1) {
+		std::cerr << "setsockopt error" << std::endl;
+		exit(ERROR_EXIT);
+		// throw std::runtime_error("setsockopt failed");
+	}
+
+	int bind_value = bind(server.sockfd, (const struct sockaddr *)&server.address, sizeof(server.address));
+	if (bind_value < 0) {
+		std::cerr << "Bind error : " << bind_value << std::endl;
+		exit(ERROR_EXIT);
+	}
+
+	int listen_value = listen(server.sockfd, server.backlog);
+	if (listen_value < 0) {
+		std::cerr << "Listen error : " << listen_value << std::endl;
+		exit(ERROR_EXIT);
+	}
+
+	server.launch = launch;
+
+	return (server);
+
+}
---
+++ src/serverSourceCode/Server.hpp
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Server.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mayoub <mayoub@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/24 13:38:06 by sihemayoub        #+#    #+#             */
+/*   Updated: 2023/07/05 16:45:59 by mayoub           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERVER_HPP
+# define SERVER_HPP
+
+# define ORANGE                      "\033[1;38;2;255;187;54m"
+# define VIOLET                      "\033[1;38;2;158;46;156m"
+# define BLUE                        "\033[1;38;2;73;153;254m"
+# define RED                         "\033[1;38;2;255;0;0m"
+# define GREEN                       "\033[1;38;2;69;176;26m"
+# define YELLOW                      "\033[1;38;2;241;223;9m"
+# define PINK                        "\033[1;38;2;240;170;223m"
+# define CYAN                        "\033[1;38;2;43;236;195m"
+# define JAUNE                       "\033[1;38;2;251;255;66"
+# define RESET                       "\033[0m"
+# define PORT                        8883
+# define TRUE                        1
+# define FALSE                       0
+# define MAXLINE                     4096
+# define MAX_EVENTS                  1024
+# define RN                          "\r\n"
+# define RNRN                        "\r\n\r\n"
+# define TIME_OUT                    6000
+
+# include <iostream>
+# include <fstream>
+
+# include <unistd.h>
+
+# include <exception>
+# include <stdexcept>
+
+# include <sys/socket.h>
+# include <netinet/in.h>
+
+# define ERROR_EXIT 1
+# define PUBLIC_DIR std::string("public")
+
+
+typedef struct Server {
+
+	int		domain;
+	int		service;
+	int		protocole;
+	u_long	interface;
+	int		port;
+	int		backlog;
+
+	struct sockaddr_in	address;
+
+	int		sockfd;
+
+	void	(*launch)(struct Server *server);
+
+}	Server;
+
+struct Server	ServerConstruct( int domain, int service, int protocole, u_long interface, int port, int backlog, void (*launch)(struct Server *server) );
+
+#endif /* Server.hpp */
